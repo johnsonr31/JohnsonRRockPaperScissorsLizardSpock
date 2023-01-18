4d64485a7034e418788128dc54d76ad67e0087af
ricRobo4 <artistr3@proton.me>
2023-01-18 13:58:00 -0800

    worked on visual design

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+// Divs that will hide or unhide
+let mainMenu = document.getElementById("mainMenu");
+let cpuOptions = document.getElementById("cpuOptions");
+
+// Buttons
+let cpuBtn = document.getElementById("cpuBtn");
+let pvpBtn = document.getElementById("pvpBtn");
+let cpu1of1Btn = document.getElementById("cpu1of1Btn");
+let cpu3of5Btn = document.getElementById("cpu3of5Btn");
+let cpu4of7Btn = document.getElementById("cpu4of7Btn");
+
+
