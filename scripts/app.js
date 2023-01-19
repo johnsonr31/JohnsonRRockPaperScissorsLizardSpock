d091891bb91a8c65984d699b003999535bb09fd9
ricRobo4 <artistr3@proton.me>
2023-01-18 23:43:35 -0800

    started on screen change event

--- scripts/app.js
+++ scripts/app.js
@@ -13,8 +13,24 @@ let cpu1of1Btn = document.getElementById("cpu1of1Btn");
 let cpu3of5Btn = document.getElementById("cpu3of5Btn");
 let cpu4of7Btn = document.getElementById("cpu4of7Btn");
 
+// Event Listeners
+cpuBtn.addEventListener("click", function(){
+    ClearScreen();
+    CpuPage();
+})
 
 // Functions
+function ClearScreen() {
+    mainMenu.innerHTML = "";
+}
+
+function CpuPage() {
+    // Create the heading for the CPU page
+    let h2 = document.createElement("h2");
+    h2.className = "cpuPageHeading";
+    h2.textContent = "CPU Match";
+}
+
 function getAPI() {
     fetch("https://scottsrpsls.azurewebsites.net/api/RockPaperScissors/GetRandomOption")
     .then((response) => response.text())
